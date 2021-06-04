53ad40740353d95deba0b50b46db51fa146d5ea0
Morten Just <morten.just@genstart.dk>
2021-06-04 13:04:22 +0200

    Initial Commit

---
+++ Device Recording Demo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Device Recording Demo
+//
+//  Created by Morten Just on 6/4/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
